7e138d2e61ffb3a6e3c74d14c72a0d1d8f452b0d
Tuhinbadhon <tuhinbadhon@gmail.com>
2025-12-26 18:17:12 +0600

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 3000,
+    historyApiFallback: true,
   },
 });
